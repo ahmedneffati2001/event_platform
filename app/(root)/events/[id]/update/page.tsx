b3c1b6263bf15617a3d4395db978096f58b6e9bf
ahmedneffati2001 <150074855+ahmedneffati2001@users.noreply.github.com>
2025-01-30 00:25:08 +0100

    Update page.tsx

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,19 +1,13 @@
 import EventForm from "@/components/shared/EventForm"
 import { getEventById } from "@/lib/actions/event.actions"
-import { auth } from "@clerk/nextjs";
+import { getAuth } from "@clerk/nextjs/server"
 
 type UpdateEventProps = {
-  params: {
-    id: string
-  }
+  userId: string;
+  event: any;
 }
 
-const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
-  const { sessionClaims } = auth();
-
-  const userId = sessionClaims?.userId as string;
-  const event = await getEventById(id)
-
+const UpdateEvent = ({ userId, event }: UpdateEventProps) => {
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -25,11 +19,39 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
           type="Update" 
           event={event} 
           eventId={event._id} 
-          userId={userId} 
+          userId={userId} // Passez userId à votre composant EventForm
         />
       </div>
     </>
   )
 }
 
-export default UpdateEvent
+// Utilisation de getServerSideProps pour récupérer le userId côté serveur
+export const getServerSideProps = async ({ req, params }: any) => {
+  try {
+    // Récupérer l'utilisateur authentifié côté serveur
+    const { userId } = getAuth(req);  // Utilisation de getAuth pour obtenir la session
+
+    if (!userId) {
+      return { 
+        redirect: { destination: "/login", permanent: false } // Rediriger si non authentifié
+      };
+    }
+
+    const event = await getEventById(params.id); // Récupérer l'événement par ID
+
+    return {
+      props: {
+        userId,  // Passez userId en tant que props
+        event,   // Passez les données de l'événement
+      },
+    }
+  } catch (error) {
+    console.error("Erreur d'authentification ou récupération de l'événement", error);
+    return { 
+      redirect: { destination: "/login", permanent: false } // En cas d'erreur, rediriger
+    };
+  }
+}
+
+export default UpdateEvent;
